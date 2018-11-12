37a0a9cefd513014609414addfd42d8f587cb9dd
MistrKraus <petahousik@gmail.com>
2018-11-12 20:20:24 +0100

    PepeHands

--- learning.c
+++ learning.c
@@ -17,53 +17,26 @@
  * @param array array to be resized
  * @param arr_len length of array
  * @param offset length to be added to array
- * @return size_t new array length
+ * @return  new array length
  */
-size_t resize(char **array, size_t arr_len, size_t offset) {
+char **resize(char **array, size_t arr_len, size_t offset) {
     size_t new_length = arr_len + offset;
 
-    char **temp = (char **) malloc(sizeof(char *)*arr_len);
-    if (!temp) {
-        printf("Malloc error!\n");
-        return 0;
-    }
+    char **temp = (char **) realloc(array, arr_len + offset);
 
-    size_t i = 0;
-    for (i; i < arr_len; i++)
-        temp[i] = array[i];
 
-    array = (char **) realloc(array, new_length);
-
-    if (!array) {
-        printf("Realloc error!\n");
-        return 0;
-    }
-
-    for (i = 0; i < arr_len; i++) {
-        array[i] = temp[i];
-    }
-
-    for (int j = (unsigned int) arr_len; j < new_length; ++j) {
-        array[j] = temp[arr_len - 1];
-    }
-
-    free(temp);
-    temp = NULL;
-    //free(temp);
-
-// ? Alternative ? //
 
 //    char **temp = (char **) malloc(sizeof(char *)*new_length);
+//    if (!temp)
+//        return temp;
 //
 //    size_t i = 0;
 //    for (i; i < arr_len; i++)
 //        temp[i] = array[i];
 //
-//    free(*array);
+//    free(array);
 //
-//    array = temp;
-
-    return new_length;
+//    return temp;
 }
 
 //FILE *load_word(char *word, FILE *file, int c) {
@@ -83,7 +56,6 @@ size_t resize(char **array, size_t arr_len, size_t offset) {
 
 void load_words_mls(FILE *file, int mls) {
     printf("File found! %d\n", mls);
-
     // ASCII of char in file
     int c;
     // index of  char in string
@@ -91,13 +63,13 @@ void load_words_mls(FILE *file, int mls) {
     // size of array
     size_t arr_size = DEF_ARR_SIZE;
     // array of words
-    char *full_words = (char *) malloc(sizeof(char *)*arr_size);
+    char **full_words = (char **) malloc(sizeof(char *)*arr_size);
     // number of words in array
     size_t word_num = 0;
     //printf("%d\n", (int)(*(&full_words + 1) - full_words));
     c = fgetc(file);
 
-    while (c != EOF && word_num < DEF_ARR_SIZE) {// && word_num < 300) {// && arr_size < DEF_ARR_SIZE) {
+    while (c != EOF && word_num < 200) {// && arr_size < DEF_ARR_SIZE) {
         //full_words[arr_size][0] = (char *) malloc(sizeof(char) * WORD_LEN);
         char *word = (char *) malloc(sizeof(char) * WORD_LEN);
         if (!word) {
@@ -117,15 +89,20 @@ void load_words_mls(FILE *file, int mls) {
         word_num++;
 
 //        printf("[%d/%d]%s\n", (unsigned int) word_num, (unsigned int) arr_size, full_words[word_num - 1]);
-//        if (word_num == arr_size) {
-//            printf("-> Reallocking memory!\n");
-//            arr_size = resize(full_words, arr_size, DEF_ARR_SIZE);
-//            printf("New array size is %d\n", (unsigned int) arr_size);
-//
-//            if (arr_size == 0) {
-//                printf("-> Realloc error!\n");
-//            }
-//        }
+        if (word_num == arr_size) {
+            printf("-> Reallocking memory!\n");
+//            full_words = resize(full_words, arr_size, DEF_ARR_SIZE);
+            char **full2 = (char **) realloc(full_words, sizeof(char *)*(arr_size*2));
+            if (!full2) {
+                printf("Malloc error!\n");
+                free(full_words);
+                return;
+            }
+            full_words = full2;
+
+            arr_size = arr_size * 2;
+            printf("New array size is %d\n", (unsigned int) arr_size);
+        }
 
         while (c < 65) {
             c = fgetc(file);
@@ -133,8 +110,6 @@ void load_words_mls(FILE *file, int mls) {
         //printf("+\n");
     }
     printf("Words loaded\n");
-
-    return;
 
 
     fclose(file);
